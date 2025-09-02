99319aa00e9e269bae3c928b92a9226f10b55c09
arjit203 <arjitgupta76@gmail.com>
2025-09-03 01:04:36 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ export const authDataContext=createContext()
 
 function AuthContext({children}) {
 
-    let serverUrl="http://localhost:8000"
+    let serverUrl="https://onecart-backend-iqap.onrender.com"
 
     let value={
         serverUrl

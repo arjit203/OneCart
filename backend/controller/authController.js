c7d4346fa6bcf562a8b28f26a6c6d5a5d0b98308
arjit203 <arjitgupta76@gmail.com>
2025-09-03 01:01:50 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -29,8 +29,8 @@ export const registration = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -61,8 +61,8 @@ export const login= async (req,res)=>{
         let token= await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -102,8 +102,8 @@ export const googleLogin = async (req, res) => {
         let token = await genToken(user._id);
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -127,8 +127,8 @@ export const adminLogin = async (req,res) => {
         let token = await genToken1(email)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 1 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(token)
